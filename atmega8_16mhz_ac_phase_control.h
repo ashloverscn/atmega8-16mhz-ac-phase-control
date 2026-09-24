8f9140ebc89a679c1ef0027d2dcfccd3d0fc31c3
ashloverscn <ashloverscn@gmail.com>
2026-09-24 19:59:15 +0530

    Update atmega8_16mhz_ac_phase_control.h

--- atmega8_16mhz_ac_phase_control.h
+++ atmega8_16mhz_ac_phase_control.h
@@ -1,16 +1,14 @@
-
 #ifndef atmega8_16mhz_ac_phase_control_h
 #define atmega8_16mhz_ac_phase_control_h
 
 class atmega8_16mhz_ac_phase_controlClass
 {
   public:
-      	void init();
-		void uninit();
-		void set_ac_power(int value);
+    void init();
+    void uninit();
+    void set_ac_power(int value);
 };
 
 extern atmega8_16mhz_ac_phase_controlClass atmega8_16mhz_ac_phase_control;
 
 #endif
-
